ea77ec00c4fd6495516a684a14627e2b21db2192
Edita2 <edita.bark@icloud.com>
2022-07-14 15:56:51 +0300

    validation and reseting

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -3,18 +3,50 @@ import Card from "./Card.js";
 import classes from "./Input.module.css";
 import "./Input.module.css";
 import Button from "./Button";
+import { useState } from "react";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    setEnteredUsername("");
+    setEnteredAge("");
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (enteredAge < 0) {
+      return;
+    }
   };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form className="input" onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button />
       </form>
     </Card>
